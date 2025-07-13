2168cf8c7b2fba2f766634f24bd1f0f69334748f
hienonichan <hvu6582@gmail.com>
2025-07-13 17:02:43 +0700

    remove members realtime

--- src/main/java/com/hien/back_end_app/dto/request/SocketDeleteMemberRequestDTO.java
+++ src/main/java/com/hien/back_end_app/dto/request/SocketDeleteMemberRequestDTO.java
@@ -1,6 +1,5 @@
 package com.hien.back_end_app.dto.request;
 
-
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +10,7 @@ import java.util.Set;
 @Setter
 @Getter
 @Builder
-public class SocketAddMemberRequestDTO {
-    @NotEmpty(message = "adding users must not be empty")
+public class SocketDeleteMemberRequestDTO {
+    @NotEmpty(message = "deleting users must not be empty")
     private Set<Long> ids;
 }
