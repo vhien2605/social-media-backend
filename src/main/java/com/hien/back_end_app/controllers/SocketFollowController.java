0a817bfa59306ef862b4eebe89a0f1749df492c8
hienonichan <hvu6582@gmail.com>
2025-07-20 22:57:15 +0700

    follow realtime

--- src/main/java/com/hien/back_end_app/controllers/SocketFollowController.java
+++ src/main/java/com/hien/back_end_app/controllers/SocketFollowController.java
@@ -1,9 +1,20 @@
 package com.hien.back_end_app.controllers;
 
 
+import com.hien.back_end_app.services.SocketFollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SocketFollowController {
-    
+    private final SocketFollowService socketFollowService;
+
+    @MessageMapping("/follow/follow-to/{userId}")
+    public void followTo(@DestinationVariable Long userId, SimpMessageHeaderAccessor accessor) {
+        socketFollowService.followTo(userId, accessor);
+    }
 }
