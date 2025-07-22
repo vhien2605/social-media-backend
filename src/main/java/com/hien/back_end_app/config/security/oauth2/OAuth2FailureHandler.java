d18d171706d49024dd2153df9231d14ff8af7d02
hienonichan <hvu6582@gmail.com>
2025-07-22 12:38:47 +0700

    update oauth2 failure handler, change throw exception because AppException advice not work in security filter

--- src/main/java/com/hien/back_end_app/config/security/oauth2/OAuth2FailureHandler.java
+++ src/main/java/com/hien/back_end_app/config/security/oauth2/OAuth2FailureHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +19,14 @@ import java.io.IOException;
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        OAuth2AuthenticationException ex = (OAuth2AuthenticationException) exception;
         response.setStatus(HttpStatus.OK.value());
         response.setContentType("application/json");
         ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-                .status(ErrorCode.UNAUTHORIZED.getCode())
-                .message(ErrorCode.UNAUTHORIZED.getMessage())
+                .status(Integer.parseInt(ex.getError().getErrorCode()))
+                .message(ex.getMessage())
                 .path(request.getRequestURI())
-                .error(ErrorCode.UNAUTHORIZED.name())
+                .error(ex.getError().getDescription())
                 .build();
         ObjectMapper objectMapper = new ObjectMapper();
         response.getWriter().write(objectMapper.writeValueAsString(apiErrorResponse));
