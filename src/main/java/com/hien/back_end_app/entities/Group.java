efd3e269cd1dd78793619debed030704c5d990fb
hienonichan <hvu6582@gmail.com>
2025-07-22 22:21:05 +0700

    create entity group

---
+++ src/main/java/com/hien/back_end_app/entities/Group.java
@@ -0,0 +1,38 @@
+package com.hien.back_end_app.entities;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.tool.schema.spi.SchemaTruncator;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "group")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    @NotBlank(message = "group name must not be blank")
+    private String name;
+
+    @Column(name = "description")
+    @NotBlank(message = "group description must not be blank")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User createdBy;
+
+    @OneToMany(mappedBy = "group")
+    private Set<GroupUser> groupUsers;
+}
---
+++ src/main/java/com/hien/back_end_app/entities/GroupUser.java
@@ -0,0 +1,31 @@
+package com.hien.back_end_app.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "group_user")
+public class GroupUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+}
